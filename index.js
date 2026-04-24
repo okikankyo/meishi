cf046840056f9be0b3a76411885aca02a21129e4
okikankyo <okikankyo@gmail.com>
2026-04-24 09:19:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 app.use(express.json({ limit: '20mb' }));
 
-console.log('🔥 起動確認');
+console.log('🔥 名刺管理くん 起動確認');
 
 app.get('/', (req, res) => {
+  console.log('GET確認');
   res.send('名刺管理くん稼働中');
 });
 
 app.post('/', (req, res) => {
-  console.log('📩 受信:', JSON.stringify(req.body));
+  console.log('📩 受信:', JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
